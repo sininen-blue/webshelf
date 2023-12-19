7e9a8e67efe6460da79726c56db39dd077e413d3
sininen-blue <alaanneil@gmail.com>
2023-12-19 23:37:22 +0800

    impl: desk app

--- webshelf/webshelf/settings.py
+++ webshelf/webshelf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "desk.apps.DeskConfig",
     "shelf.apps.ShelfConfig",
     "books.apps.BooksConfig",
     'django.contrib.admin',
